57f960fa59e6e583d7a181277f2b868c69cb3eaf
Paul Clarke <paulioclarke@icloud.com>
2023-08-26 21:40:28 +1000

    help

--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -3,19 +3,22 @@ import './nav.css';
 
 const Nav = () => {
     // const nav = useNavigate();
+    const restart = () => window.location.reload()
 
     return (
         <nav className="appNav">
             <Tooltip title="Back to Main Menu">
-            <div className="logo" onClick={() => window.location.reload()}>
-                <Typography variant="overline" display="block" component="h2" sx={{textAlign: 'center'}}>
-                    STUDY DUEL
-                </Typography>
-            </div>
+                <div className="logo" onClick={restart}>
+                    <Typography variant="overline" display="block" component="h2" sx={{textAlign: 'center'}}>
+                        STUDY DUEL
+                    </Typography>
+                </div>
             </Tooltip>
-            {/* <div>
-                New Game
-            </div> */}
+            <Tooltip title="Start a New Game">
+                <div className="newGame" onClick={restart}>
+                    <span>New Game</span>
+                </div>
+            </Tooltip>
         </nav>
     )
 }
